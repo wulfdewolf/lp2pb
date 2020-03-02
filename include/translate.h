99ecc431d7007ab66e0ebd3dd4d73a271fbceee7
wulfdewolf <wolf.de.wulf@vub.be>
2020-03-02 14:20:48 +0100

    added the translator class, this class will have separate methods for each different category of rules --> some will call lp2sat!

---
+++ include/translate.h
@@ -0,0 +1,24 @@
+#ifndef TRANSLATE_H
+#define TRANSLATE_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+// TRANSLATOR CLASS
+class Translator {
+
+    private:
+
+    int amount_of_variables = 0;
+    int amount_of_contraints = 0;
+
+
+    public: 
+
+};
+
+#endif
