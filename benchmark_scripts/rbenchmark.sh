1130b9f45027d8d52eddfbbb70d60fc5256443b7
wulfdewolf <wolf.de.wulf@vub.be>
2020-04-02 14:23:16 +0200

    added a lock to the result.csv file in the benchmark scripts, otherwise cores could interleave their result writings

--- benchmark_scripts/rbenchmark.sh
+++ benchmark_scripts/rbenchmark.sh
@@ -11,6 +11,11 @@ module load libreadline/8.0-GCCcore-8.3.0
 
 ../../runlim -o runlim_INSTANCE_SETUP.txt -r 1500 -s 14336 SCRIPT ARG >> solver_INSTANCE_SETUP.txt
 
+
+## LOCK
+exec {lock_fd}>../../lock/mylockfile || exit 1
+flock -n "$lock_fd"
+ 
 # solution
 if cat solver_INSTANCE_SETUP.txt | grep -E "OPTIMUM";
 then 
@@ -52,6 +57,9 @@ printf '%s' "FAMILY," >> ../../result.csv;
 
 # instance
 printf "INSTANCE\n" >> ../../result.csv;
+ 
+## UNLOCK
+flock -u "$lock_fd"
 
 # remove temp files
 rm runlim_INSTANCE_SETUP.txt
